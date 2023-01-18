fe8b5d3e279980021c78120e1400097e0a398abf
Melania P Chagas <melaniapchagas@gmail.com>
2023-01-18 17:37:55 -0300

    Criando classe 'Car' que representa o objeto do carro

---
+++ src/Domains/Car.ts
@@ -0,0 +1,54 @@
+import ICar from '../Interfaces/ICar';
+
+class Car {
+  /* 
+    "O 'protected' age de forma semelhante ao 'private', com a exceção de que os membros declarados 'protected' também podem ser acessados dentro das classes derivadas."
+    https://www.typescriptlang.org/docs/handbook/classes.html
+  */
+
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status: boolean | undefined;
+  protected buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor(car: ICar) {
+    this.id = car.id;
+    this.model = car.model;
+    this.year = car.year;
+    this.color = car.color;
+    this.status = car.status;
+    this.buyValue = car.buyValue;
+    this.doorsQty = car.doorsQty;
+    this.seatsQty = car.seatsQty;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public getModel() {
+    return this.model;
+  }
+
+  public getyear() {
+    return this.year;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }
+}
+
+export default Car;
